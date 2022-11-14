924b165a8a59feb614aff36d3a4072602d236111
jairdev <devalfredomoscoso@gmail.com>
2022-11-14 14:32:28 -0400

    Refactor signout endpoint

--- pages/api/singout.ts
+++ pages/api/singout.ts
@@ -1,12 +1,10 @@
 import { withIronSessionApiRoute } from "iron-session/next";
+import { NextApiRequest, NextApiResponse } from "next";
+import { withSessionRoute } from "../../lib/sessions";
 
-const cookie = {
-  cookieName: process.env.COOKIE_NAME,
-  password: process.env.COOKIE_PASSWORD,
-  cookieOptions: { secure: process.env.NODE_ENV === "production" },
-};
+export default withSessionRoute(signOut);
 
-export default withIronSessionApiRoute(function signOut(req, res) {
+function signOut(req: NextApiRequest, res: NextApiResponse) {
   req.session.destroy();
   res.status(200).json({ ok: true });
-}, cookie);
+}
