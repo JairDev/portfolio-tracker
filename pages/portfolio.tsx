566537861e8280534d7499182d3eb36a52d5d980
jairdev <devalfredomoscoso@gmail.com>
2023-02-02 12:53:47 -0400

    Fix errors

--- pages/portfolio.tsx
+++ pages/portfolio.tsx
@@ -55,6 +55,7 @@ export default function Porfolio({ data = [] }: { data: PortfolioProps }) {
   const { spacing } = useTheme();
 
   const { authenticated, coins, coinData } = data;
+  console.log(authenticated);
   const [totalAmount, setTotalAmount] = useState(0);
   const [open, setOpen] = useState(false);
 
@@ -97,112 +98,112 @@ export default function Porfolio({ data = [] }: { data: PortfolioProps }) {
     setUserData(resultFlatUserData);
   }, [coinData, coinDataApi, coins]);
 
-  if (!authenticated) {
-    return (
-      <div>
-        <Box
-          sx={{
-            display: "flex",
-            paddingTop: spacing(14),
-          }}
-        >
-          <Box
-            sx={{
-              width: "40%",
-            }}
-          >
-            <Typography
-              variant="caption"
-              sx={{ fontWeight: "700", color: "primary.main" }}
-            >
-              Regístrese hoy
-            </Typography>
+  // if (!authenticated) {
+  //   return (
+  //     <div>
+  //       <Box
+  //         sx={{
+  //           display: "flex",
+  //           paddingTop: spacing(14),
+  //         }}
+  //       >
+  //         <Box
+  //           sx={{
+  //             width: "40%",
+  //           }}
+  //         >
+  //           <Typography
+  //             variant="caption"
+  //             sx={{ fontWeight: "700", color: "primary.main" }}
+  //           >
+  //             Regístrese hoy
+  //           </Typography>
 
-            <Typography variant="h4" sx={{ fontWeight: "700" }}>
-              Rastreador de cartera criptográfica
-            </Typography>
-            <Box maxWidth="600px" sx={{ marginTop: spacing(3) }}>
-              <Typography variant="subtitle1" sx={{ color: " #B6B6B6" }}>
-                Realice un seguimiento de sus ganancias y pérdidas con nuestra
-                plataforma fácil de usar.
-              </Typography>
-            </Box>
-            <Box sx={{ marginTop: spacing(8) }}>
-              <Link
-                component={NextLink}
-                underline="none"
-                color="text.primary"
-                sx={{
-                  bgcolor: "primary.main",
-                  padding: spacing(2, 5),
-                  borderRadius: "8px",
-                  fontWeight: "600",
-                }}
-                href="/register"
-              >
-                Crea tu portafolio
-              </Link>
-            </Box>
-          </Box>
-          <Box
-            sx={{
-              position: "relative",
-              marginTop: spacing(10),
-              transform: "translateX(20%)",
-            }}
-          >
-            <Image
-              src={screenApp}
-              alt="Imagen de un portafolio de criptomonedas"
-            />
-          </Box>
-        </Box>
-      </div>
-    );
-  }
+  //           <Typography variant="h4" sx={{ fontWeight: "700" }}>
+  //             Rastreador de cartera criptográfica
+  //           </Typography>
+  //           <Box maxWidth="600px" sx={{ marginTop: spacing(3) }}>
+  //             <Typography variant="subtitle1" sx={{ color: " #B6B6B6" }}>
+  //               Realice un seguimiento de sus ganancias y pérdidas con nuestra
+  //               plataforma fácil de usar.
+  //             </Typography>
+  //           </Box>
+  //           <Box sx={{ marginTop: spacing(8) }}>
+  //             <Link
+  //               component={NextLink}
+  //               underline="none"
+  //               color="text.primary"
+  //               sx={{
+  //                 bgcolor: "primary.main",
+  //                 padding: spacing(2, 5),
+  //                 borderRadius: "8px",
+  //                 fontWeight: "600",
+  //               }}
+  //               href="/register"
+  //             >
+  //               Crea tu portafolio
+  //             </Link>
+  //           </Box>
+  //         </Box>
+  //         <Box
+  //           sx={{
+  //             position: "relative",
+  //             marginTop: spacing(10),
+  //             transform: "translateX(20%)",
+  //           }}
+  //         >
+  //           <Image
+  //             src={screenApp}
+  //             alt="Imagen de un portafolio de criptomonedas"
+  //           />
+  //         </Box>
+  //       </Box>
+  //     </div>
+  //   );
+  // }
 
-  if (authenticated && userData.length <= 0) {
-    return (
-      <Box
-        sx={{
-          flexDirection: "column ",
-          justifyContent: "center ",
-          alignItems: "center ",
-          top: 0,
-          left: 0,
-          width: "100%",
-          zIndex: "10",
-        }}
-      >
-        <Box
-          sx={{
-            paddingTop: spacing(10),
-            display: "flex",
-            flexDirection: "column ",
-            justifyContent: "center ",
-            alignItems: "center ",
-          }}
-        >
-          <Typography sx={{ fontSize: "24px", fontWeight: "500" }}>
-            Este portafolio está vacío
-          </Typography>
-          <Typography sx={{ marginTop: "8px" }}>
-            Agregue cualquier moneda para comenzar
-          </Typography>
-          <Box sx={{ marginTop: "32px" }}>
-            <Button
-              onClick={handleClickAddCoin}
-              text="Añadir nueva moneda"
-              variant="contained"
-            >
-              <AddCircleOutlineIcon />
-            </Button>
-          </Box>
-          <BasicModal open={open} setOpen={setOpen} />
-        </Box>
-      </Box>
-    );
-  }
+  // if (authenticated && userData.length <= 0) {
+  //   return (
+  //     <Box
+  //       sx={{
+  //         flexDirection: "column ",
+  //         justifyContent: "center ",
+  //         alignItems: "center ",
+  //         top: 0,
+  //         left: 0,
+  //         width: "100%",
+  //         zIndex: "10",
+  //       }}
+  //     >
+  //       <Box
+  //         sx={{
+  //           paddingTop: spacing(10),
+  //           display: "flex",
+  //           flexDirection: "column ",
+  //           justifyContent: "center ",
+  //           alignItems: "center ",
+  //         }}
+  //       >
+  //         <Typography sx={{ fontSize: "24px", fontWeight: "500" }}>
+  //           Este portafolio está vacío
+  //         </Typography>
+  //         <Typography sx={{ marginTop: "8px" }}>
+  //           Agregue cualquier moneda para comenzar
+  //         </Typography>
+  //         <Box sx={{ marginTop: "32px" }}>
+  //           <Button
+  //             onClick={handleClickAddCoin}
+  //             text="Añadir nueva moneda"
+  //             variant="contained"
+  //           >
+  //             <AddCircleOutlineIcon />
+  //           </Button>
+  //         </Box>
+  //         <BasicModal open={open} setOpen={setOpen} />
+  //       </Box>
+  //     </Box>
+  //   );
+  // }
 
   return (
     <Box
