b3ecc1f1cdb28b096fc5a79cd52b1044ebddb552
jairdev <devalfredomoscoso@gmail.com>
2023-02-03 11:40:06 -0400

    Not build errors

--- lib/getServerSideProps.ts
+++ lib/getServerSideProps.ts
@@ -12,38 +12,34 @@ export const withGetServerSideProps = async function ({ req }) {
   const api_server = "http://localhost:3000";
   let userSession = req?.session?.user;
 
-  try {
-    const res = await fetch(`${api_server}/api/auth`, {
-      method: "POST",
-      headers: {
-        Accept: "application/json",
-        "Content-Type": "application/json",
+  const res = await fetch(`${api_server}/api/auth`, {
+    method: "POST",
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
 
-        Authorization: `Bearer ${userSession?.token}`,
-      },
-    });
+      Authorization: `Bearer ${userSession?.token}`,
+    },
+  });
 
-    const userData = await res.json();
-    const coinsLastPrice = userData.coins.map(
-      async (coin: CoinsLastPriceArgument) => {
-        const coinData = await getApiCoinData(coin.name);
-        return coinData;
-      }
-    );
+  const userData = await res.json();
+  const coinsLastPrice = userData.coins.map(
+    async (coin: CoinsLastPriceArgument) => {
+      const coinData = await getApiCoinData(coin.name);
+      return coinData;
+    }
+  );
 
-    const resultAllCoinsData = await Promise.all(coinsLastPrice);
-    console.log(resultAllCoinsData);
+  const resultAllCoinsData = await Promise.all(coinsLastPrice);
+  console.log(resultAllCoinsData);
 
-    const data = JSON.parse(
-      JSON.stringify({ ...userData, coinData: resultAllCoinsData })
-    );
+  const data = JSON.parse(
+    JSON.stringify({ ...userData, coinData: resultAllCoinsData })
+  );
 
-    return {
-      props: {
-        data,
-      },
-    };
-  } catch (error) {
-    console.log(error);
-  }
+  return {
+    props: {
+      data,
+    },
+  };
 };
--- pages/portfolio.tsx
+++ pages/portfolio.tsx
@@ -52,15 +52,16 @@ interface CoinFilter {
 //@ts-ignore
 export default function Porfolio({ data = [] }: { data: PortfolioProps }) {
   // const { data: coinDataApi } = useSWR(urlCoin);
-  const [mounted, setMounted] = useState(false);
+  const [mounted, setMounted] = useState(true);
   const { spacing } = useTheme();
+
   useEffect(() => {
-    setMounted(true);
+    setMounted(false);
   }, []);
 
-  if (!mounted) {
-    return null;
-  }
+  // if (!mounted) {
+  //   return null;
+  // }
 
   // const { authenticated, coins, coinData } = data;
   // console.log(authenticated);
@@ -212,42 +213,42 @@ export default function Porfolio({ data = [] }: { data: PortfolioProps }) {
   //     </Box>
   //   );
   // }
-
-  return (
-    <Box
-      sx={{
-        display: "flex",
-        marginTop: spacing(14),
-      }}
-    >
-      <Box sx={{ width: "100%" }}>
-        <Box
-          sx={{
-            display: "flex",
-            justifyContent: "space-between",
-            position: "relative",
-            zIndex: " 50",
-          }}
-        >
-          <Box>
-            <Typography sx={{ fontSize: "14px" }}>Balance actual</Typography>
-            <Typography sx={{ fontSize: "24px", fontWeight: "bold" }}>
-              {/* ${formatCurrency(totalAmount, "usd")} */}
-            </Typography>
+  if (!mounted) {
+    return (
+      <Box
+        sx={{
+          display: "flex",
+          marginTop: spacing(14),
+        }}
+      >
+        <Box sx={{ width: "100%" }}>
+          <Box
+            sx={{
+              display: "flex",
+              justifyContent: "space-between",
+              position: "relative",
+              zIndex: " 50",
+            }}
+          >
+            <Box>
+              <Typography sx={{ fontSize: "14px" }}>Balance actual</Typography>
+              <Typography sx={{ fontSize: "24px", fontWeight: "bold" }}>
+                {/* ${formatCurrency(totalAmount, "usd")} */}
+              </Typography>
+            </Box>
+            <Box>
+              <Button
+                // onClick={handleClickAddCoin}
+                text="Añadir nueva moneda"
+                variant="contained"
+              >
+                <AddCircleOutlineIcon />
+              </Button>
+            </Box>
+            {/* <BasicModal open={open} setOpen={setOpen} /> */}
           </Box>
-          <Box>
-            <Button
-              // onClick={handleClickAddCoin}
-              text="Añadir nueva moneda"
-              variant="contained"
-            >
-              <AddCircleOutlineIcon />
-            </Button>
-          </Box>
-          {/* <BasicModal open={open} setOpen={setOpen} /> */}
-        </Box>
-        <Box sx={{ marginTop: spacing(10) }}>
-          {/* {userData.length > 0 && (
+          <Box sx={{ marginTop: spacing(10) }}>
+            {/* {userData.length > 0 && (
             <Typography
               variant="h5"
               sx={{ marginBottom: "16px", fontWeight: "bold" }}
@@ -256,7 +257,7 @@ export default function Porfolio({ data = [] }: { data: PortfolioProps }) {
             </Typography>
           )} */}
 
-          {/* {!userData[0] ? (
+            {/* {!userData[0] ? (
             <Box
               sx={{
                 display: "flex",
@@ -272,9 +273,10 @@ export default function Porfolio({ data = [] }: { data: PortfolioProps }) {
             </>
           )} */}
 
-          <Box sx={{ marginTop: "16px" }}></Box>
+            <Box sx={{ marginTop: "16px" }}></Box>
+          </Box>
         </Box>
       </Box>
-    </Box>
-  );
+    );
+  }
 }
