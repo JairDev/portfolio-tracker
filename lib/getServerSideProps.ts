c3359cdfbc82d130657dcd885a75e1ac03c22cde
jairdev <devalfredomoscoso@gmail.com>
2023-02-04 12:14:58 -0400

    Test build

--- lib/getServerSideProps.ts
+++ lib/getServerSideProps.ts
@@ -22,20 +22,21 @@ export const withGetServerSideProps = async function ({ req }) {
     },
   });
 
-  const userData = await res.json();
-  const coinsLastPrice = userData.coins.map(
-    async (coin: CoinsLastPriceArgument) => {
-      const coinData = await getApiCoinData(coin.name);
-      return coinData;
-    }
-  );
+  // const userData = await res.json();
+  // const coinsLastPrice = userData.coins.map(
+  //   async (coin: CoinsLastPriceArgument) => {
+  //     const coinData = await getApiCoinData(coin.name);
+  //     return coinData;
+  //   }
+  // );
 
-  const resultAllCoinsData = await Promise.all(coinsLastPrice);
-  console.log(resultAllCoinsData);
+  // const resultAllCoinsData = await Promise.all(coinsLastPrice);
+  // console.log(resultAllCoinsData);
 
-  const data = JSON.parse(
-    JSON.stringify({ ...userData, coinData: resultAllCoinsData })
-  );
+  // const data = JSON.parse(
+  //   JSON.stringify({ ...userData, coinData: resultAllCoinsData })
+  // );
+  const data = {};
 
   return {
     props: {
