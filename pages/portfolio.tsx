b7eb149147f3ab26ce85f2d209d42ad6d0c731c8
jairdev <devalfredomoscoso@gmail.com>
2023-02-06 12:18:12 -0400

    Look for error in portfolio component production

--- pages/portfolio.tsx
+++ pages/portfolio.tsx
@@ -311,15 +311,15 @@ export const getServerSideProps = async function ({ req }) {
   const api_server = "http://localhost:3000";
   let userSession = req?.session?.user;
   // // console.log(userSession);
-  // const res = await fetch(`${api_server}/api/auth`, {
-  //   method: "POST",
-  //   headers: {
-  //     Accept: "application/json",
-  //     "Content-Type": "application/json",
+  const res = await fetch(`${api_server}/api/auth`, {
+    method: "POST",
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
 
-  //     Authorization: `Bearer ${userSession?.token}`,
-  //   },
-  // });
+      Authorization: `Bearer ${userSession?.token}`,
+    },
+  });
 
   // const userData = await res.json();
   // //@ts-ignore
