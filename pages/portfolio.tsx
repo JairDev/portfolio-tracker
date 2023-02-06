093d4eee2a18368d44848ebd46dadf1cf9eb0c09
jairdev <devalfredomoscoso@gmail.com>
2023-02-06 12:10:27 -0400

    Look for error in portfolio component production

--- pages/portfolio.tsx
+++ pages/portfolio.tsx
@@ -309,7 +309,7 @@ const sessionOptions: IronSessionOptions = {
 //@ts-ignore
 export const getServerSideProps = async function ({ req }) {
   const api_server = "http://localhost:3000";
-  // let userSession = req?.session?.user;
+  let userSession = req?.session?.user;
   // // console.log(userSession);
   // const res = await fetch(`${api_server}/api/auth`, {
   //   method: "POST",
