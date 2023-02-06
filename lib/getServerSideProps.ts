de9c615907da92707f293f59e350907a328137f8
jairdev <devalfredomoscoso@gmail.com>
2023-02-06 12:57:32 -0400

    Look for error in portfolio component production

--- lib/getServerSideProps.ts
+++ lib/getServerSideProps.ts
@@ -11,36 +11,40 @@ interface CoinsLastPriceArgument {
 export const withGetServerSideProps = async function ({ req }) {
   const api_server = "http://localhost:3000";
   let userSession = req?.session?.user;
+  try {
+    const res = await fetch(`${api_server}/api/auth`, {
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
 
-  // const res = await fetch(`${api_server}/api/auth`, {
-  //   method: "POST",
-  //   headers: {
-  //     Accept: "application/json",
-  //     "Content-Type": "application/json",
+        Authorization: `Bearer ${userSession?.token}`,
+      },
+    });
 
-  //     Authorization: `Bearer ${userSession?.token}`,
-  //   },
-  // });
+    const userData = await res.json();
+    const coinsLastPrice = userData.coins.map(
+      async (coin: CoinsLastPriceArgument) => {
+        const coinData = await getApiCoinData(coin.name);
+        return coinData;
+      }
+    );
 
-  // const userData = await res.json();
-  // const coinsLastPrice = userData.coins.map(
-  //   async (coin: CoinsLastPriceArgument) => {
-  //     const coinData = await getApiCoinData(coin.name);
-  //     return coinData;
-  //   }
-  // );
+    const resultAllCoinsData = await Promise.all(coinsLastPrice);
+    console.log(resultAllCoinsData);
 
-  // const resultAllCoinsData = await Promise.all(coinsLastPrice);
-  // console.log(resultAllCoinsData);
+    const data = JSON.parse(
+      JSON.stringify({ ...userData, coinData: resultAllCoinsData })
+    );
 
-  // const data = JSON.parse(
-  //   JSON.stringify({ ...userData, coinData: resultAllCoinsData })
-  // );
-  const data = {};
-
-  return {
-    props: {
-      data,
-    },
-  };
+    return {
+      props: {
+        data,
+      },
+    };
+  } catch (error) {
+    return {
+      props: {},
+    };
+  }
 };
