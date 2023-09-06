3fb1feb3e23868cae85268bc70d38ab9792767be
jairdev <devalfredomoscoso@gmail.com>
2023-09-06 19:00:31 -0400

    Fix database connection error

--- lib/useCryptoData.ts
+++ lib/useCryptoData.ts
@@ -4,6 +4,7 @@ import { urlCoin } from "./apiUrl";
 export function useCryptoData() {
   const { data: cryptoData } = useSWR(urlCoin);
   if (cryptoData) {
+    // @ts-ignore
     const formatData = cryptoData.map((object) => {
       return { id: object.id, name: object.name };
     });
